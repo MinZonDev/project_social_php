5b24128ac3a2da05f2ae5ecdb23c6100fa9d2787
MinZon <trongpro0307@gmail.com>
2024-04-05 22:34:34 +0700

    base

--- app/assets/layout/header.php
+++ app/assets/layout/header.php
@@ -1,62 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chirp</title>
-    <link rel="stylesheet" href="../app/assets/css/styles.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
-    <!-- sidebar starts -->
-    <div class="sidebar">
-      <i class="fab fa-twitter"></i>
-      <div class="sidebarOption active">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Chirp</title>
+  <link rel="stylesheet" href="../app/assets/css/styles.css" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+    crossorigin="anonymous" />
+</head>
+
+<body>
+  <!-- sidebar starts -->
+  <div class="sidebar">
+    <i class="fab fa-twitter"></i>
+    <div class="sidebarOption active">
+      <a href="http://localhost/project_social_php/public/index.php?controller=HomeController&action=index"class="sidebarLink">
         <span class="material-icons"> home </span>
         <h2>Home</h2>
-      </div>
+      </a>
+    </div>
 
-      <div class="sidebarOption">
-        <span class="material-icons"> search </span>
-        <h2>Explore</h2>
-      </div>
+    <div class="sidebarOption">
+      <span class="material-icons"> search </span>
+      <h2>Explore</h2>
+    </div>
 
-      <div class="sidebarOption">
-        <span class="material-icons"> notifications_none </span>
-        <h2>Notifications</h2>
-      </div>
+    <div class="sidebarOption">
+      <span class="material-icons"> notifications_none </span>
+      <h2>Notifications</h2>
+    </div>
 
-      <div class="sidebarOption">
-        <span class="material-icons"> mail_outline </span>
-        <h2>Messages</h2>
-      </div>
+    <div class="sidebarOption">
+      <span class="material-icons"> mail_outline </span>
+      <h2>Messages</h2>
+    </div>
 
-      <div class="sidebarOption">
-        <span class="material-icons"> bookmark_border </span>
-        <h2>Bookmarks</h2>
-      </div>
+    <div class="sidebarOption">
+      <span class="material-icons"> bookmark_border </span>
+      <h2>Bookmarks</h2>
+    </div>
 
-      <div class="sidebarOption">
-        <span class="material-icons"> list_alt </span>
-        <h2>Lists</h2>
-      </div>
+    <div class="sidebarOption">
+      <span class="material-icons"> list_alt </span>
+      <h2>Lists</h2>
+    </div>
 
-      <div class="sidebarOption">
+    <div class="sidebarOption">
+      <a href="http://localhost/project_social_php/public/index.php?controller=ProfileController&action=show"class="sidebarLink">
         <span class="material-icons"> perm_identity </span>
         <h2>Profile</h2>
-      </div>
-
-      <div class="sidebarOption">
-        <span class="material-icons"> more_horiz </span>
-        <h2>More</h2>
-      </div>
-      <button class="sidebar__tweet">Tweet</button>
+      </a>
     </div>
-    <!-- sidebar ends -->
+
+
+    <div class="sidebarOption">
+      <span class="material-icons"> more_horiz </span>
+      <h2>More</h2>
+    </div>
+    <button class="sidebar__tweet">Tweet</button>
+  </div>
+  <!-- sidebar ends -->
--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -1,31 +1,25 @@
-<?php
-include '../app/assets/layout/header.php';
-?>
+<?php include '../app/assets/layout/header.php'; ?>
+
 <div class="container">
-    <h1>User Profile</h1>
-    <p>Welcome,
-        <?php echo $data['username']; ?>!
-    </p>
-    <div class="profile-info">
-        <p><img class="avatar" src="../app/assets/images/<?php echo $data['avatar']; ?>" alt="User Avatar"> </p>
-        <p>Email:
-            <?php echo $data['email']; ?>
-        </p>
-        <p><span>Bio:</span>
-            <?php echo $data['bio']; ?>
-        </p>
-        <p><span>Location:</span>
-            <?php echo $data['location']; ?>
-        </p>
-        <p><span>Website:</span>
-            <?php echo $data['website']; ?>
-        </p>
-        <p><span>Date Joined:</span>
-            <?php echo $data['datejoined']; ?>
-        </p>
+    <!-- Top section for user profile -->
+    <div class="profile-section">
+        <h1>User Profile</h1>
+        <p>Welcome, <?php echo $data['username']; ?>!</p>
+        <div class="profile-info">
+            <p><img class="avatar" src="../app/assets/images/<?php echo $data['avatar']; ?>" alt="User Avatar"></p>
+            <p>Email: <?php echo $data['email']; ?></p>
+            <p><span>Bio:</span> <?php echo $data['bio']; ?></p>
+            <p><span>Location:</span> <?php echo $data['location']; ?></p>
+            <p><span>Website:</span> <?php echo $data['website']; ?></p>
+            <p><span>Date Joined:</span> <?php echo $data['datejoined']; ?></p>
+        </div>
+        <a href="http://localhost/project_social_php/public/index.php?controller=ProfileController&action=edit" class="edit-button">Edit Profile</a>
     </div>
-    <button class="edit-button">Edit Profile</button>
+
+    <!-- Bottom section for user posts -->
+    <div class="feed">
+        <!-- Add user posts here -->
+    </div>
 </div>
-<?php
-include '../app/assets/layout/footer.php';
-?>
+
+<?php include '../app/assets/layout/footer.php'; ?>

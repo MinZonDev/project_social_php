328d3d10e5b83e7b3e8dc6c5dd9b000805ab21f6
MinZon <trongpro0307@gmail.com>
2024-04-17 00:42:57 +0700

    chat hoan thien nhung mac ke toi can di ngu

--- socket/server.php
+++ socket/server.php
@@ -9,54 +9,34 @@ use Ratchet\WebSocket\WsServer;
 
 class Chat implements MessageComponentInterface {
     protected $clients;
-    protected $db;
-    
+    protected $usernames;
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
-        // Thay thế các giá trị bên dưới bằng thông tin kết nối cơ sở dữ liệu thực tế của bạn
-        $this->db = new mysqli('chirp-social-minzondev.b.aivencloud.com:28999', 'avnadmin', 'AVNS_hUaIHWb2H3eQt_qspQG', 'chirp_social');
-        if ($this->db->connect_error) {
-            die("Connection failed: " . $this->db->connect_error);
-        }
+        $this->usernames = [];
     }
 
     public function onOpen(ConnectionInterface $conn) {
-
         $this->clients->attach($conn);
         echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        $messageData = json_decode($msg, true);
-        $receiver = $messageData['receiver'];
-        $content = $messageData['content'];
-
-        // Save message to database
-        $this->saveMessage($messageData['sender'], $receiver, $content);
-
-        foreach ($this->clients as $client) {
-            if ($client !== $from) {
-                continue;
-            }
-
-            if ($client->resourceId == $receiver) {
-                $client->send($msg);
-                break;
+        $data = json_decode($msg, true);
+        if ($data['action'] === 'setName') {
+            $this->usernames[$from->resourceId] = $data['name'];
+        } elseif ($data['action'] === 'message') {
+            foreach ($this->clients as $client) {
+                if ($client !== $from) {
+                    $client->send(json_encode(['action' => 'message', 'sender' => $this->usernames[$from->resourceId], 'message' => $data['message']]));
+                }
             }
         }
     }
 
-    protected function saveMessage($senderID, $receiverID, $content) {
-        // Insert message into the database
-        $stmt = $this->db->prepare("INSERT INTO messages (SenderID, ReceiverID, Content) VALUES (?, ?, ?)");
-        $stmt->bind_param("iis", $senderID, $receiverID, $content);
-        $stmt->execute();
-        $stmt->close();
-    }
-
     public function onClose(ConnectionInterface $conn) {
         $this->clients->detach($conn);
+        unset($this->usernames[$conn->resourceId]);
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
